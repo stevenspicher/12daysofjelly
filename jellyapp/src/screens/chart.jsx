dc0ae11231e87bb8aaabf1a42f6a776c9fc9c812
steven <steven.spicher@gmail.com>
2023-12-13 11:41:44 -0700

    added comments

--- jellyapp/src/screens/chart.jsx
+++ jellyapp/src/screens/chart.jsx
@@ -1,164 +1,123 @@
 import React, {useEffect, useState} from "react";
 import Button from "react-bootstrap/Button";
-import {Container, Row, Col, Table} from "react-bootstrap";
+import {Row, Col, Table} from "react-bootstrap";
 import {useLocation, useNavigate} from "react-router-dom";
 import {Grid} from '@mui/material'
 import {getConsole, getState, getUsers, editUser} from "../shared/utilities";
 import {jellyList} from "../shared/containers";
 import Header from "../components/Header"
 import SpeedDial from "../components/SpeedDial"
+import {Chart} from "react-google-charts";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import {Container, Grow, Paper, Stack} from "@mui/material";
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import Collapse from '@mui/material/Collapse';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
+import Divider from '@mui/material/Divider';
 
 const RatingsChart = (props) => {
-    console.log(props)
+    console.log(props.state.userList)
     const navigate = useNavigate();
     const location = useLocation();
     const [tableUserList, setTableUserList] = useState(props.state.userList)
+    const elevationHeight = 12;
+    const ExpandMore = styled((props) => {
+        const { expand, ...other } = props;
+        return <IconButton {...other} />;
+    })(({ theme, expand }) => ({
+        transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    }));
+    const [expanded, setExpanded] = useState(false);
 
-
-    const apricotTable =
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+    const options = {
+        legend: "none",
+        is3D: true,
+        fontSize: 15,
+        bar: {groupWidth: "10%"},
+    };
+    const chartData = [
+        ["Name", "Rating"]
+    ];
+    const cardData = [];
+    const JellyData = (jellyName, userList, jellyIndex) => {
         props.state.userList.map((user, userIndex) => {
-            const index = 1
-            return (
-                <tr key={userIndex}>
-                    <td>{user.name}</td>
-                    <td>{user.jellies[index].rating}</td>
-                    <td>{user.jellies[index].comments}</td>
-                </tr>
-                )
-            })
+            if (user.jellies[jellyIndex].rating !== undefined) {
+            chartData.push([user.name, user.jellies[jellyIndex].rating])
+            }
+            if (user.jellies[jellyIndex].comments !== undefined) {
+            cardData.push([user.name, user.jellies[jellyIndex].comments])
+            }
+        })
 
-    // const cherriesTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 2
-    //         return (
-    //             <tr key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const yuzuTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 9
-    //         return (
-    //             <tr key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const plumTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 8
-    //         return (
-    //             <tr   key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const honeyTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 6
-    //         return (
-    //             <tr   key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const figTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 4
-    //         return (
-    //             <tr   key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const rhubarbTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 11
-    //         return (
-    //             <tr   key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const dragonFruitTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 5
-    //         return (
-    //             <tr   key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const chestnutTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 3
-    //         return (
-    //             <tr   key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const mangoTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 7
-    //         return (
-    //             <tr   key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const aniseTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 12
-    //         console.log(user.jellies[index])
-    //         return (
-    //             <tr   key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
-    //
-    // const peachTable =
-    //     props.state.userList.map((user, userIndex) => {
-    //         const index = 10
-    //         return (
-    //             <tr   key={userIndex}>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.jellies[index].rating}</td>
-    //                 <td>{user.jellies[index].deliveryMethod}</td>
-    //             </tr>
-    //         )
-    //     })
+            return (
+                <Grow in={true} timeout={1000}>
+                    <Paper elevation={elevationHeight} sx={{marginTop: "10px", border: '1px solid black'}}>
+                        <Container>
+                            <Card>
+
+                                <CardHeader  title={jellyName}/>
+                                <CardMedia
+                                    component="img"
+                                    height="194"
+                                    image={jellyList[jellyIndex - 1].image}
+                                    alt={jellyName}
+                                    />
+
+                            <Chart
+                                chartType="BarChart"
+                                width="100%"
+                                height="100px"
+                                data={chartData}
+                                options={options}
+                            />
+                                <Divider/>
+                                <Typography sx={{marginTop: "20px"}}>Comments</Typography>
+                                <ExpandMore
+                                    expand={expanded}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </ExpandMore>
+                                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                    <CardContent>
+
+                            {cardData.map((user, index) => {
+                                return (
+                                    <Card key={index} sx={{minWidth: 275}}>
+                                    <CardContent>
+                                        <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                            {user[0]}
+                                        </Typography>
+                                        <Typography variant="h5" component="div">
+                                            {user[1]}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                                )
+                            })}
+                                    </CardContent>
+                                </Collapse>
+                            </Card>
+                        </Container>
+                    </Paper>
+                </Grow>
+            )
+        }
 
     useEffect(() => {
         getUsers(props.state.setUserList)
@@ -166,137 +125,12 @@ const RatingsChart = (props) => {
 
     }, []);
     return (
-            <Container>
-                <SpeedDial/>
-                <Table  size="sm">
-                    <thead>
-                    <tr>
-                        <th className="title-signup" colSpan={3}>
-                    <Grid alignContent='flex-start' alignItems='flex-start' justify='flex-start'>
-                    <img
-                        style={{
-                            height: "100px",
-                            width: "100px"
-                        }}
-                        src={jellyList[0].image}/>
-                    </Grid>
-                            Apricot and Banana
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {apricotTable}
-                    </tbody>
-                </Table>
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={3}>Cherries and ElderFlower</th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>*/}
-                {/*    {cherriesTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={3}>Orange, Yuzu, and Grapefruit </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>*/}
-                {/*    {yuzuTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={3}>Mirabelle Plum with Spice </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>*/}
-                {/*    {plumTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={6}>Honey </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>*/}
-                {/*    {honeyTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={4}>Fig with Cardamom </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>*/}
-                {/*    {figTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={4}>Rhubarb and Strawberry </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>*/}
-                {/*    {rhubarbTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={4}>Grapefruit and Dragonfruit </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>{dragonFruitTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={4}>Chestnut with Orange and Spice  </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>{chestnutTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={4}>Mango, Raspberry, and Lime  </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>{mangoTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={4}>Strawberry with Star Anise  </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>{aniseTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-                {/*<Table size="sm">*/}
-                {/*    <thead>*/}
-                {/*    <tr>*/}
-                {/*        <th className="title-signup" colSpan={4}>Peach with Jasmine </th>*/}
-                {/*    </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>{peachTable}*/}
-                {/*    </tbody>*/}
-                {/*</Table>*/}
-            </Container>
+        <Container>
+            <SpeedDial/>
+            {JellyData("Apricot-Banana", props.state.userList, 1)}
+        </Container>
 
-
-        )
+    )
 };
 
 export default RatingsChart;
